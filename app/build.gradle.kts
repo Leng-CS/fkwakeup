a9919d9ac284cbdf9b644b9776fc210f216b0f0a
Leng-CS <clkeeponm@gmail.com>
2026-09-23 22:29:39 +0800

    chore(app): 设置新图标并美化项目首页

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.lengcs.fkwakeup"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.0"
+        versionCode = 4
+        versionName = "1.0.1"
     }
 
     buildTypes {
